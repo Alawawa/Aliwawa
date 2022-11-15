63217bf632f0ed5ccaf72297e9fad14536b8f70e
Tim Kang <timkilyoonkang@gmail.com>
2022-11-14 19:06:26 -0800

    added mockdata for graphQL

---
+++ server/controllers/itemController.js
@@ -0,0 +1,8 @@
+const db = require('../models/itemModel');
+const itemController = {};
+
+itemController.getItems = (req, res, next) => {
+  // get items functionality
+}
+
+module.exports = itemController;
