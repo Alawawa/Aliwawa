43e31796304baaf5eaace1a840680433a40c9914
Jaitnium <matthewaantosiak@gmail.com>
2022-11-16 16:23:51 -0800

    Preparing for final version

--- __tests__/client.test.js
+++ __tests__/client.test.js
@@ -1,28 +1,72 @@
-import React from 'React';
-import App from '../client/components/App';
-import Body from '../client/components/Body';
-import Search from '../client/components/Search';
-import Navbar from '../client/components/Navbar';
-import Login from '../client/components/Login';
-import Register from '../client/components/Register';
-import ShoppingCart from '../client/components/ShoppingCart';
-import Marketplace from '../client/components/Marketplace';
+// import React from 'React';
+// import { render, screen, waitFor } from '@testing-library/react';
 
-import { render, screen, waitFor } from '@testing-library/react';
+// import App from '../client/components/App';
+// import Body from '../client/components/Body';
+// import Search from '../client/components/Search';
+// import Navbar from '../client/components/Navbar';
+// import Login from '../client/components/Login';
+// import Register from '../client/components/Register';
+// import ShoppingCart from '../client/components/ShoppingCart';
+// import Marketplace from '../client/components/Marketplace';
 
-describe('Client Tests', () => {
+// describe('Client Tests', () => {
 
-  let app
+//   let app;
 
-  beforeAll(async () => {
-    app = await render(
-      <App />
-    )
-  });
+//   beforeAll(async () => {
+//     // app = await render(
+//     //   <App />
+//     // )
+//   });
 
-  describe('Home page'), () => {
-    it('User should be able to log in with their new account', async () => {
-      
-    })
-  }
-});
+//   const graphQLPost = async (queryString, params = {}) => {
+
+//     return await fetch(APP + "api", {
+//       method: "POST",
+//       body: JSON.stringify({
+//         query: queryString,
+//         variables: params
+//       }),
+//       headers: {
+//         "Content-Type": "application/json",
+//         "Access-Control-Allow-Origin": "*",
+//       },
+//     })
+//       .then((res) => {
+//         return res.json();
+//       })
+//       .then((data) => {
+//         //console.log('data resp:', data);
+//         return data;
+//       })
+//       .catch((err) => {
+//         //console.log('ERROR!', err);
+//         return err;
+//       });
+//   }  
+
+//   describe('Home page'), () => {
+//     it('Confirm each database entry is being displayed', async () => {
+//       const resp = await graphQLPost(`query  {
+//         getAllListings {
+//           id
+//           itemName
+//           itemPrice
+//           itemDesc
+//           itemPic
+//           tags
+//           purchased
+//           seller {
+//             username
+//             email
+//           }
+//         }
+//       }`, {});
+//       let allEntries = resp.data.getAllListings;
+//       for(let i = 0; i < allEntries.length; i++) {
+//         console.log(screen.getByText(allEntries[i].itemName));
+//       }
+//     })
+//   }
+// });
